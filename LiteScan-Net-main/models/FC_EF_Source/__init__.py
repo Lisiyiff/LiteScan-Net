570483c486e3bfc374abe18c183a49398e4a3f58
Lisiyiff <Lisiyiff@gmail.com>
2026-04-29 10:38:21 +0800

    Delete LiteScan-Net-main directory

--- LiteScan-Net-main/models/FC_EF_Source/__init__.py
+++
@@ -1,6 +0,0 @@
-from .unet import Unet as FC_EF
-from .siamunet_conc import SiamUnet_conc as FC_Siam_conc
-from .siamunet_diff import SiamUnet_diff as FC_Siam_diff
-
-# 这样你在外面只需要：
-# from models.comparison.FC_EF_Source import FC_EF, FC_Siam_conc, FC_Siam_diff
